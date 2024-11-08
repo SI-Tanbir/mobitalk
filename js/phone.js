a2f9db80e76c123461fd9244e525ae39fa3349ee
SI-Tanbir <si017490@gmail.com>
2024-11-08 16:58:02 +0600

    finnaly finished this dummy

--- js/phone.js
+++ js/phone.js
@@ -1,50 +1,95 @@
-// fetch('https://jsonplaceholder.typicode.com/photos')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
 
-// Define the async function to use await
 async function fetchData() {
-  let res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  let res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
   let data = await res.json();
+let phones = data.data;
 
-  data.forEach((item) => {
-    //     console.log(`ID: ${item.id}, Title: ${item.title}, URL: ${item.url}`);
-    //
-    // console.log(item);
-//  starting of card here
-    let phoneContainer = document.getElementById("phone-container");
-    let div = document.createElement("div");
-    div.innerHTML = `
-<div class="card bg-base-100 w-96 shadow-xl">
-  <figure class="px-10 pt-10">
-    <img
-      src="${item.thumbnailUrl}"
-      alt="Shoes"
-      class="rounded-xl" />
-  </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">${item.title}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
+
+
+
+
+//displaying phone here
+function phonedisplay(phones){
+    phones.forEach((phone) =>{
+
+
+
+
+        /* 
+        brand: "Apple "
+    ​
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg"
+    ​
+    phone_name: "iPhone 13 mini"
+    ​
+    slug: "apple_iphone_13_mini-11104"
+        
+        */
+     
+    
+        let phoneContainer = document.getElementById("phone-container");
+        let div = document.createElement("div");
+        div.innerHTML = `
+    <div class="card  w-96 bg-[#877cda40] mt-4 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img
+          src="${phone.image}"
+          alt="Shoes"
+          class="rounded-xl" />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        
+        <div class="card-actions">
+          <button class="btn bg-[orange]">Buy Now</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
+    
+    `;
+    
+        phoneContainer.appendChild(div);
+    
+    
+      });
+}
+// phonedisplay(phones)
 
-`;
+if (phones.length >= 9){
+    let device=phones.slice(0,9);
+    console.log('it seems working',device);
+    phonedisplay(device);
+    let showall =document.getElementById('showall')
+    showall.classList.remove('hidden')
 
-    phoneContainer.appendChild(div);
+     
 
-    // formate i just copy for reference
+}
+else{
+    console.log('slicking not work')
+    phonedisplay(phones);
+    let showall =document.getElementById('showall');
+    showall.classList.add('hidden');
+    
+}
 
-    //   albumId: 1,
-    //   id: 1,
-    //   title: 'accusamus beatae ad facilis cum similique qui sunt',
-    //   url: 'https://via.placeholder.com/600/92c952',
-    //   thumbnailUrl: 'https://via.placeholder.com/150/92c952'
-  });
+// function displayFull(){
+//     let showall =document.getElementById('showall');
+//     showall.classList.add('hidden');
+//     phonedisplay(phones);
 
-  // console.log(data.)
+
+// };
+
+let displaybtn = document.getElementById('showall');
+displaybtn.addEventListener('click', function() {
+    let showall = document.getElementById('showall');
+    showall.classList.add('hidden');
+    phonedisplay(phones);
+});
+
+
+
 }
 
 // Call the async function to execute the fetch operation
